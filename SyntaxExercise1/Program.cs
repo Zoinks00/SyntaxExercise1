65c80b0b155486ebac85787095af71e624006269
Forrest Rabideau <forrest.rabideau@gmail.com>
2021-11-08 11:31:41 -0800

    1st commit

---
+++ SyntaxExercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SyntaxExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less then 9." : $"{answer} greater than or equal to nine";
+              
+            Console.WriteLine(response);
+        }
+    }
+}
